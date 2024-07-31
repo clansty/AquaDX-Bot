5e5d79036d0ee58587840ef19f46a66f202de310
Clansty <i@gao4.pw>
2024-07-31 14:23:54 +0800

    feat: 使用按钮切换 profile

--- apps/workers/src/bot/bind.ts
+++ apps/workers/src/bot/bind.ts
@@ -2,6 +2,7 @@ import { Telegraf } from 'telegraf';
 import BotContext from './BotContext';
 import { Env } from '../types';
 import { SdgbProxied, UserProfile } from '@clansty/maibot-clients';
+import _ from 'lodash';
 
 export default (bot: Telegraf<BotContext>, env: Env) => {
 	const handleQueryBind = async (ctx: BotContext) => {
@@ -10,7 +11,7 @@ export default (bot: Telegraf<BotContext>, env: Env) => {
 		if (profiles.length) {
 			bond += `\n\n现在已经绑定 ${profiles.length} 个账号\n使用 /profile 命令来查看已经绑定的账号`;
 		}
-		await ctx.reply('用法: /bind <ID 或 Chime>' + bond);
+		await ctx.reply('用法: /bind <AquaDX 的“账户卡”号码> 或 <国服微信二维码识别出来的文字>' + bond);
 	};
 
 	bot.start(async (ctx, next) => {
@@ -19,6 +20,11 @@ export default (bot: Telegraf<BotContext>, env: Env) => {
 	});
 
 	bot.command('bind', async (ctx) => {
+		if (ctx.chat.type !== 'private') {
+			await ctx.reply('请在私聊中使用此命令');
+			return;
+		}
+
 		const profiles = await ctx.getProfiles();
 
 		if (ctx.args.length < 1) {
@@ -56,6 +62,22 @@ export default (bot: Telegraf<BotContext>, env: Env) => {
 		await ctx.reply(text);
 	});
 
+	const getProfilesText = async (ctx: BotContext) => {
+		const profiles = await ctx.getProfiles();
+		const current = await ctx.getCurrentProfile(false);
+
+		const text = await Promise.all(
+			profiles.map(async (p, i) => {
+				const text = `${i + 1}. ${p.type} ${p.userId}`;
+				if (p === current) {
+					return `<b>${text}</b> (选中)`;
+				}
+				return text;
+			}));
+
+		return text.join('\n');
+	};
+
 	bot.command(['profile', 'profiles'], async (ctx) => {
 		const profiles = await ctx.getProfiles();
 		if (!profiles.length) {
@@ -68,16 +90,14 @@ export default (bot: Telegraf<BotContext>, env: Env) => {
 				await ctx.reply('请在私聊中使用此命令');
 				return;
 			}
-			const text = await Promise.all(
-				profiles.map(async (p, i) => {
-					const text = `${i + 1}. ${p.type} ${p.userId}`;
-					if (p === await ctx.getCurrentProfile(false)) {
-						return `<b>${text}</b> (选中)`;
-					}
-					return text;
-				}));
-
-			await ctx.replyWithHTML(text.join('\n'));
+			await ctx.replyWithHTML(await getProfilesText(ctx), {
+				reply_markup: {
+					inline_keyboard: _.chunk(profiles.map((_, i) => ({
+						text: (i + 1).toString(),
+						callback_data: `select_profile:${i}`
+					})), 4)
+				}
+			});
 			return;
 		}
 
@@ -89,5 +109,20 @@ export default (bot: Telegraf<BotContext>, env: Env) => {
 
 		await ctx.selectProfile(index);
 		await ctx.reply('切换成功');
+	});
+
+	bot.action(/^select_profile:(\d+)$/, async (ctx) => {
+		const index = Number(ctx.match[1]);
+		await ctx.selectProfile(index);
+		await ctx.answerCbQuery('切换成功');
+		await ctx.editMessageText(await getProfilesText(ctx), {
+			parse_mode: 'HTML',
+			reply_markup: {
+				inline_keyboard: _.chunk((await ctx.getProfiles()).map((_, i) => ({
+					text: (i + 1).toString(),
+					callback_data: `select_profile:${i}`
+				})), 4)
+			}
+		});
 	});
 }
